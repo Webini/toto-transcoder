456f1fb6495e6b142ff841bd580fd4b85350a735
Nicolas <nicolas.gasp@free.fr>
2016-12-31 02:11:06 +0100

    reflection

--- src/manager.js
+++ src/manager.js
@@ -21,6 +21,29 @@ class Manager {
    *        v             v
    *  [ Transcoder A ] [ Transcoder B ]
    *  ( ^ Transcode preset received from transcoding Queue )
+   * 
+   * Better hypothesis 
+   *            [ RabbitMq ]
+   *              /      \
+   *    [ File Queue ] [ Transcoding Queue ] <= replaceable by an std event emitter
+   *          |                 ^        |      but we loose maxInstances with the prefetch
+   *          v                 |        |      opt of rabbit and worker queue, so duplication
+   *     [ Manager ] ------------        |      of messages
+   *                ( ^ calculate and    |
+   *                    send presets )   |
+   *                                     |
+   *     ---------------------------------
+   *     v                      v
+   *   [ Transcoder A ]  [ Transcoder B ] <= we can put evt emitter here but we have to 
+   *                                         reimplement the worker logic in the manager
+   *                                         to dispatch messages one by one to the free
+   *                                         transcoder
+   * 
+   * We have to add a notification queue for transcoding progress
+   * the manager must not handle transcoder configuration or maybe remove
+   * it, focus only on transcoder, and let the devs play as they want
+   * Then we can implement our microservice with Media and Transcoder
+   * KISS
    */
   transcode(media) {
 
